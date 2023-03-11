e919d04ee6a5b9faf95938bab1e38fe801bcd197
Stanislav Demyanovich <s.demyanovich@protonmail.com>
2023-03-11 18:31:02 +0400

    Add more unit-tests for argument and argument_list parsing

--- sources/libpdcli/ut/argument_tests.cpp
+++ sources/libpdcli/ut/argument_tests.cpp
@@ -13,6 +13,7 @@ TEST_CASE("argument parsing tests" "[clidget][argument]")
 {
     argument<std::string> str_arg{};
     argument<int> int_arg{};
+    argument<long> long_arg{};
     argument<unsigned long> ulong_arg{};
 
     std::vector<std::string> cmd_line
@@ -21,7 +22,10 @@ TEST_CASE("argument parsing tests" "[clidget][argument]")
         "22"s,
         "-33"s,
         "+44"s,
-        "1001100100"s
+        "1234567890"s,
+        "-1234567890"s,
+        "1001100100"s,
+        "-1001100100"s
     };
 
     gnu_style st{};
@@ -42,18 +46,59 @@ TEST_CASE("argument parsing tests" "[clidget][argument]")
     REQUIRE(cursor == 2u);
     REQUIRE(int_arg.is_given());
     REQUIRE(int_arg.value() == 22);
+
     int_arg = argument<int>{};
+    REQUIRE(!int_arg.is_given());
     success = int_arg.parse_cmd_line(cmd_line, cursor, st);
     REQUIRE(success);
     REQUIRE(cursor == 3u);
     REQUIRE(int_arg.is_given());
     REQUIRE(int_arg.value() == -33);
-    int_arg = argument<int>();
+
+    int_arg = argument<int>{};
+    REQUIRE(!int_arg.is_given());
     success = int_arg.parse_cmd_line(cmd_line, cursor, st);
-    REQUIRE(success == true);
+    REQUIRE(success);
     REQUIRE(cursor == 4u);
     REQUIRE(int_arg.is_given() == true);
     REQUIRE(int_arg.value() == 44);
+
+    REQUIRE(!long_arg.is_given());
+    success = long_arg.parse_cmd_line(cmd_line, cursor, st);
+    REQUIRE(success);
+    REQUIRE(cursor == 5u);
+    REQUIRE(long_arg.is_given());
+    REQUIRE(long_arg.value() == 1234567890);
+
+    long_arg = argument<long>{};
+    REQUIRE(!long_arg.is_given());
+    success = long_arg.parse_cmd_line(cmd_line, cursor, st);
+    REQUIRE(success);
+    REQUIRE(cursor == 6u);
+    REQUIRE(long_arg.is_given());
+    REQUIRE(long_arg.value() == -1234567890);
+
+    REQUIRE(!ulong_arg.is_given());
+    success = ulong_arg.parse_cmd_line(cmd_line, cursor, st);
+    REQUIRE(success);
+    REQUIRE(cursor == 7u);
+    REQUIRE(ulong_arg.is_given());
+    REQUIRE(ulong_arg.value() == 1001100100u);
+
+    ulong_arg = argument<unsigned long>{};
+    REQUIRE(!ulong_arg.is_given());
+    success = ulong_arg.parse_cmd_line(cmd_line, cursor, st);
+    REQUIRE(!success);
+    REQUIRE(cursor == 7u);
+    REQUIRE(!ulong_arg.is_given());
+
+    cmd_line = {};
+    int_arg = argument<int>{};
+    cursor = 0u;
+    success = int_arg.parse_cmd_line(cmd_line, cursor, st);
+    REQUIRE(!success);
+    REQUIRE(cursor == 0u);
+    REQUIRE(!int_arg.is_given());
 }
 
 TEST_CASE("argument_list parsing tests", "[clidget][argument_list]")
@@ -70,6 +115,7 @@ TEST_CASE("argument_list parsing tests", "[clidget][argument_list]")
     gnu_style st{};
     bool success{false};
     std::size_t cursor{0u};
+
     argument_list<std::string> str_arg_list{};
 
     REQUIRE(!str_arg_list.is_given());
@@ -80,4 +126,31 @@ TEST_CASE("argument_list parsing tests", "[clidget][argument_list]")
     REQUIRE(str_arg_list.value().size() == 5u);
     REQUIRE(str_arg_list.value()[0u] == "arg-0"s);
     REQUIRE(str_arg_list.value()[4u] == "arg-4"s);
+
+    argument_list<std::string> str_arg_list2{"", "", 1u, 5u};
+    cursor = 0u;
+    success = str_arg_list2.parse_cmd_line(cmd_line, cursor, st);
+    REQUIRE(success);
+    REQUIRE(cursor == 5u);
+    REQUIRE(str_arg_list.is_given());
+    REQUIRE(str_arg_list.value().size() == 5u);
+    REQUIRE(str_arg_list.value()[0u] == "arg-0"s);
+    REQUIRE(str_arg_list.value()[4u] == "arg-4"s);
+
+    argument_list<std::string> str_arg_list3{"", "", 5u, 50u};
+    cursor = 0u;
+    success = str_arg_list3.parse_cmd_line(cmd_line, cursor, st);
+    REQUIRE(success);
+    REQUIRE(cursor == 5u);
+    REQUIRE(str_arg_list.is_given());
+    REQUIRE(str_arg_list.value().size() == 5u);
+    REQUIRE(str_arg_list.value()[0u] == "arg-0"s);
+    REQUIRE(str_arg_list.value()[4u] == "arg-4"s);
+
+    argument_list<std::string> str_arg_list4{"", "", 6u, 6u};
+    cursor = 0u;
+    success = str_arg_list4.parse_cmd_line(cmd_line, cursor, st);
+    REQUIRE(!success);
+    REQUIRE(!str_arg_list4.is_given());
+    REQUIRE(cursor == 0u);
 }
